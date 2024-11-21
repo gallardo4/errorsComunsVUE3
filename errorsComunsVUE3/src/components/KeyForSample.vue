62ba11560a418921b0757e0e821c31bdad7e5e77
gallardo4 <arnau.gallardo.7e7@itb.cat>
2024-11-21 17:42:16 +0100

    Commit

--- errorsComunsVUE3/src/components/KeyForSample.vue
+++ errorsComunsVUE3/src/components/KeyForSample.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { shuffle } from 'lodash'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const items = ref([
   { id: 1, label: 'first name' },
@@ -9,16 +9,19 @@ const items = ref([
   { id: 4, label: 'website' }
 ])
 
+const text = ref('')
+watch(text, (newText, oldText) => console.log(newText, oldText))
+
 function shuffleItems() {
   items.value = shuffle(items.value)
 }
 </script>
 
 <template>
-  <ul>
-    <li v-for="item in items" :key="item.id">{{ item.label }} <!-- Afegim :key="item.id" per fixar un item -->
-        <input type="text" />
-    </li>
-  </ul>
+  <!-- <ul>
+    <li v-for="item in items" :key="item.id">{{ item.label }} Afegim :key="item.id" per fixar un item -->
+        <input type="text" v-model="text" />
+    <!-- </li>
+  </ul> -->
   <button @click="shuffleItems">Shuffle</button>
 </template>
--- errorsComunsVUE3/src/components/WatchArrayExample.vue
+++ errorsComunsVUE3/src/components/WatchArrayExample.vue
@@ -1,9 +1,8 @@
 <script setup>
 import { ref, watch } from 'vue'
-const numbers = ref([])
 
-watch(numbers, () => console.log('new number added'))
-watch(numbers, (newValue, oldValue) => console.log(`${newValue} added`))
+const numbers = ref([])
+watch(numbers, (numbers) => console.log(numbers), { deep: true })
 
 const randomNum = () => numbers.value.push(Math.random())
 </script>
